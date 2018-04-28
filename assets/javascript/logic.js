63012e9553b69444c99c578d6c1e0d458c00a824
unknown <benjamin.rademacher@gmail.com>
2018-04-28 08:17:14 -0500

    Final Push before due

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -16,22 +16,43 @@ var database = firebase.database();
 
 var trainName = "";
 var trainDestination = "";
-var frequency = 0;
-var nextArrival = "" //this will be a set time;
-var minutesAway = 0; //this is based on frequency and next arrival time
+var departureTime = "";
+var departureConverted = "";
+var currentTime = "";
+var frequency = "";
+var nextArrival = "";
+var remainder = "";
+var minutesAway = ""; //this is based on frequency and next arrival time
 
-//Function to push values to firebase database
-$(".btn").on("click", function (event) {
+//Function to push values to firebase database on click of submit button
+$(".train-btn").on("click", function (event) {
+
+    //Takes values from form and places them in variables
     trainName = $("#trainName").val().trim();
     trainDestination = $("#destination").val().trim();
-    frequency = $("#trainTime").val().trim();
+    departureTime = $("#trainTime").val().trim();
+    frequency = $("#frequency").val().trim();
 
-    // Grabbed values from text boxes
+    // Setting current time
+    currentTime = moment();
+
+    // Converting departureTime into a time value
+    departureConverted = moment(departureTime, "HH:mm");
+
+    // Getting nextArrival time
+    nextArrival = moment(departureConverted).add(minutesAway, "minutes").format("HH:mm");
+
+    // Getting minutesAway value
+    minutesAway = (moment(departureConverted)).diff(moment(), "minutes");
+
+    // Pushes values from variables to firebase
     database.ref().push({
         trainName: trainName,
         trainDestination: trainDestination,
+        departureTime: departureTime,
         frequency: frequency,
-
+        nextArrival: nextArrival,
+        minutesAway: minutesAway
     })
 
 });
@@ -44,13 +65,28 @@ database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
     console.log("Errors handled: " + errorObject.code);
 });
 
-// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+// Firebase watcher + initial loader
 database.ref().on("child_added", function (childSnapshot) {
 
     // Log everything that's coming out of snapshot
     console.log(childSnapshot.val().trainName);
     console.log(childSnapshot.val().trainDestination);
+    console.log(childSnapshot.val().departureTime);
     console.log(childSnapshot.val().frequency);
 
+    // full list of items to the well
+    $(".all-trains").append("<tr><td class='train-name'> " + childSnapshot.val().trainName +
+        " </td><td class='train-destination'> " + childSnapshot.val().trainDestination +
+        " </td><td class='train-departure'> " + childSnapshot.val().departureTime +
+        " </td><td class='train-frequency'> " + childSnapshot.val().frequency +
+        " </td><td class='train-next-arrival'> " + childSnapshot.val().nextArrival +
+        " </td><td class='train-minutes-away'> " + childSnapshot.val().minutesAway +
+        " </td></tr>");
 
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
 });
+
+
+
